e461c3bb4aef9e01da94fe66fb259f47dd3b4eee
Marcos Santana <marcos150895@gmail.com>
2019-02-02 17:13:55 -0200

    comment firehouse parquet

--- terraform/production/cloudwatch_trigger.tf
+++ terraform/production/cloudwatch_trigger.tf
@@ -1,19 +1,19 @@
-# resource "aws_cloudwatch_event_rule" "every_10_minutes_lambda" {
-#   name                = "trigger_lambda_twitter_consumer"
-#   description         = ""
-#   schedule_expression = "cron(0/10 * * * ? *)"
-# }
+resource "aws_cloudwatch_event_rule" "every_10_minutes_lambda" {
+  name                = "trigger_lambda_twitter_consumer"
+  description         = ""
+  schedule_expression = "cron(0/10 * * * ? *)"
+}
 
-# resource "aws_cloudwatch_event_target" "event_target" {
-#   rule      = "${aws_cloudwatch_event_rule.every_10_minutes_lambda.name}"
-#   target_id = "twitter_consumer"
-#   arn       = "${aws_lambda_function.twitter_consumer.arn}"
-# }
+resource "aws_cloudwatch_event_target" "event_target" {
+  rule      = "${aws_cloudwatch_event_rule.every_10_minutes_lambda.name}"
+  target_id = "twitter_consumer"
+  arn       = "${aws_lambda_function.twitter_consumer.arn}"
+}
 
-# resource "aws_lambda_permission" "cloudwatch_permission" {
-#   statement_id  = "AllowExecutionFromCloudWatch"
-#   action        = "lambda:InvokeFunction"
-#   function_name = "twitter_consumer"
-#   principal     = "events.amazonaws.com"
-#   source_arn    = "${aws_cloudwatch_event_rule.every_10_minutes_lambda.arn}"
-# }
+resource "aws_lambda_permission" "cloudwatch_permission" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "twitter_consumer"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.every_10_minutes_lambda.arn}"
+}
